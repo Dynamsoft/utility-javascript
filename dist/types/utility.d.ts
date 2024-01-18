f0c199df06f4bdda0ba9e1beaa204de5e090d511
DMGithubPublisher <github@dynamsoft.com>
2024-01-18 17:47:40 +0800

    1.0.21

--- dist/types/utility.d.ts
+++ dist/types/utility.d.ts
@@ -2,4 +2,3 @@ import ImageManager from "./class/imagemanager";
 import UtilityModule from "./class/utilitymodule";
 import MultiFrameResultCrossFilter from "./class/multiframeresultcrossfilter";
 export { ImageManager, UtilityModule, MultiFrameResultCrossFilter };
-//# sourceMappingURL=utility.d.ts.map
